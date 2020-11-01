1005d1a6663af0019881533e14ad2a1baea64e0d
22p21c0070 (Nachod ENG 48) <syscyberdyne@gmail.com>
2020-11-01 23:39:03 +0700

    Add files via upload

---
+++ M5Main/credentials.h
@@ -0,0 +1,9 @@
+/* WiFi */
+const char* WIFI_SSID = "T's iPhone";
+const char* WIFI_PASS = "poporing";
+
+/* NETPIE & MicroGear */
+char* APPID   = "M5StackDemo";
+char* KEY     = "SFqQdSX6DAcWpNo";
+char* SECRET  = "fYv1rPNdIcpbePupV4j4uO4VI";
+char* ALIAS   = "Toppppp";
